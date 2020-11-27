cac7c9cfcb4442049d68c6e2ac8d5466b5ae6543
chongwish <chongwish@gmail.com>
2020-11-27 14:52:26 +0000

    add some class method testcase

---
+++ src/test/java/me/chongwish/jjvm/ReflectionTest.java
@@ -0,0 +1,45 @@
+package me.chongwish.jjvm;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import me.chongwish.jjvm.RuntimeDataArea.MethodArea;
+import me.chongwish.jjvm.RuntimeDataArea.ThreadResource;
+
+public class ReflectionTest {
+    @Test
+    public void testClass() {
+        final List<String> classpaths = new ArrayList<>();
+        classpaths.add("./build/classes/java/test".replace('/', File.separatorChar));
+        Classpath.parse(classpaths);
+
+        ThreadResource.createThreadResource();
+
+        ClassLoader classLoader = new ClassLoader();
+
+        final String className = "me.chongwish.jjvm.ReflectionTest".replace('.', '/');
+        classLoader.load(className);
+        MethodArea.Clazz clazz = MethodArea.findClazz(className);
+        MethodArea.Method method = clazz.findMethod("getClassName", "()V");
+
+        ThreadResource threadResource = ThreadResource.getCurrentThreadResource();
+        Interpreter interpreter = Interpreter.init(threadResource);
+        interpreter.read(method).execute();
+
+    }
+
+    private void getClassName() {
+        System.out.println(int.class.getName());
+        System.out.println(int[].class.getName());
+        System.out.println(int[][].class.getName());
+        System.out.println(Integer.class.getName());
+        System.out.println(Integer[].class.getName());
+        System.out.println(Object[].class.getName());
+        System.out.println(new String[0].getClass().getName());
+        System.out.println("".getClass().getName());
+        System.out.println(new double[0].getClass().getName());
+    }
+}
